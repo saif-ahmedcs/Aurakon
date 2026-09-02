ef9f5a6b525b0846df86da281d47a6616e5c68fc
Saif Ahmed <seefahmed259@gmail.com>
2026-09-02 06:37:41 +0300

    fix: serialize demo sessions to prevent account reset race conditions

--- Backend/services/demoService.js
+++ Backend/services/demoService.js
@@ -5,9 +5,19 @@ const {
 } = require("../scripts/resetDemoAccount");
 const authService = require("./authService");
 
+let demoStartQueue = Promise.resolve();
+
+function runExclusive(fn) {
+  const run = demoStartQueue.catch(() => {}).then(fn);
+  demoStartQueue = run.catch(() => {});
+  return run;
+}
+
 async function startDemoSession() {
-  await resetDemoAccount();
-  return authService.login(DEMO_EMAIL, DEMO_PASSWORD);
+  return runExclusive(async () => {
+    await resetDemoAccount();
+    return authService.login(DEMO_EMAIL, DEMO_PASSWORD);
+  });
 }
 
 module.exports = { startDemoSession };
